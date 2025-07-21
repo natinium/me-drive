bad827ad966c46922dbd5df51d8383ced962ce80
Natnael Mesele <nmesele5@gmail.com>
2025-07-21 13:28:23 +0300

    fix(commitlint): fix the commit body length limit error

---
+++ commitlint.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [0, "always", Infinity],
+    "header-max-length": [0, 'always', Infinity]
+  },
+};
